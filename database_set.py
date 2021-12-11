6f56f382bb7f93b1196d8e618f6e862add9f664c
l3r8yJ <clicker.heroes.acg@gmail.com>
2021-12-12 01:05:04 +0300

    updated dataset

--- database_set.py
+++ database_set.py
@@ -16,6 +16,6 @@ players_template = """
 
 connection = manager.create_connection('testing_db')
 query = parser_hltv.create_query_by_table(players_url, players_template)
-# manager.execute_query(connection, query)
+manager.execute_query(connection, query)
 print(query)
 
